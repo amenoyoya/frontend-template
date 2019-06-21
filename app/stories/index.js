5ecf0cbb2b6b7ab5a9ec9cd4e389f58a30530255
amenoyoya <mtyswe3@gmail.com>
2019-06-22 00:42:18 +0900

    Fix｜絵文字ボタン削除

--- app/stories/index.js
+++ app/stories/index.js
@@ -3,9 +3,6 @@ import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
 
 storiesOf('Button', module)
-  .add('with text', () => (
-    <Button>Hello Button</Button>
-  ))
-  .add('with emoji', () => (
-    <Button><span role="img" aria-label="so cool">?? ?? ?? ??</span></Button>
-  ));   
+    .add('with text', () => (
+        <Button>Hello Button</Button>
+    ));
